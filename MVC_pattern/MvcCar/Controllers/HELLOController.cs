cc404f79d36e0cc2f0be2c55087bbc4911969ff6
tirans3 <tirans3@mail.ru>
2019-05-16 01:12:15 +0400

    Add new new method Welcome

--- MVC_pattern/MvcCar/Controllers/HELLOController.cs
+++ MVC_pattern/MvcCar/Controllers/HELLOController.cs
@@ -14,6 +14,12 @@ namespace MvcCar.Controllers
             return View();
         }
 
-       
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
     }
 }
