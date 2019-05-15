eaca45aab28dcdfe3841119077391769f000df5f
tirans3 <tirans3@mail.ru>
2019-05-16 01:28:24 +0400

    Add model car

---
+++ MVC_pattern/MvcCar/Data/MvcCarContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcCar.Models
+{
+    public class MvcCarContext : DbContext
+    {
+        public MvcCarContext (DbContextOptions<MvcCarContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcCar.Models.Car> Car { get; set; }
+    }
+}
---
+++ MVC_pattern/MvcCar/Models/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcCar.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+    }
+}
