5aef32fe747fa48a1e85946c7acb047a36da075b
tirans3 <tirans3@mail.ru>
2019-05-16 00:21:20 +0400

    Add new controller HELLO

---
+++ MVC_pattern/MvcCar/Controllers/HELLOController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCar.Controllers
+{
+    public class HELLOController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
