44eae46c8807cba6d86865625a19d4fcde10a0c8
tirans3 <tirans3@mail.ru>
2019-05-17 02:32:38 +0400

    Add attribut

--- MVC_pattern/MvcCar/Models/Car.cs
+++ MVC_pattern/MvcCar/Models/Car.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcCar.Models
 {
@@ -8,9 +9,12 @@ namespace MvcCar.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
+        [Display(Name ="Release Data")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Color { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
