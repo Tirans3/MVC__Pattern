21e4045b8be50e5fa2e3cd0a3eca9a971764bdd7
tirans3 <tirans3@mail.ru>
2019-05-18 00:39:14 +0400

    Add new migration Add price attribut,new fild

--- MVC_pattern/MvcCar/Migrations/20190517203217_Addfild.Designer.cs
+++ MVC_pattern/MvcCar/Migrations/20190517203217_Addfild.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MvcCar.Models;
 
 namespace MvcCar.Migrations
 {
     [DbContext(typeof(MvcCarContext))]
-    partial class MvcCarContextModelSnapshot : ModelSnapshot
+    [Migration("20190517203217_Addfild")]
+    partial class Addfild
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MVC_pattern/MvcCar/Migrations/20190517203217_Addfild.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcCar.Migrations
+{
+    public partial class Addfild : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Size",
+                table: "Car",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Size",
+                table: "Car");
+        }
+    }
+}
