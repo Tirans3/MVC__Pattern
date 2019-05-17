1b664989931b8039f9ad98d7caf30a6de21a48db
tirans3 <tirans3@mail.ru>
2019-05-18 01:28:39 +0400

    Add new class

---
+++ MVC_pattern/MvcCar/Models/CareSizeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MvcCar.Models
+{
+    public class CareSizeViewModel
+    {
+        public List<Car> Cars{ get; set; }
+        public SelectList Sizes { get; set; }
+        public string CarSize { get; set; }
+        public string SearchString { get; set; }
+    }
+}
+
